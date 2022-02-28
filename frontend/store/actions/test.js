a42c6c9f2a041acb2432e2c2b845ce6d7e622402
Vasco <vascbarreiros@gmail.com>
2022-02-28 10:07:47 +0100

    ajout store

---
+++ frontend/store/actions/test.js
@@ -0,0 +1,5 @@
+export const DELETE_STORE = 'DELETE_STORE';
+
+export const DeleteStore = (id) => {
+    return {type: 'DELETE_STORE', idStore: id}
+}
---
+++ frontend/store/reducers/test.js
@@ -0,0 +1,16 @@
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const ADD_PRODUCT = 'ADD_PRODUCT';
+
+export const DeleteProduct = id => {
+    return{
+        type:DELETE_PRODUCT,
+        storeId:id
+    }
+}
+
+export const AddProduct = nameProduct => {
+    return{
+        type:ADD_PRODUCT,
+        name:nameProduct
+    }
+}
