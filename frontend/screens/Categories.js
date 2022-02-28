0fb2ae84554453eb67dac07030ce35c4a1472848
Aija <dumcevaaija@gmail.com>
2022-02-28 23:05:31 +0100

    map on itemtopiclist, for now hardcoded

--- frontend/screens/Categories.js
+++ frontend/screens/Categories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity, Image, Dimensions, ScrollView } from 'react-native';
 import StyleGuide from "../style/styleGuide";
 import { LinearGradient } from "expo-linear-gradient";
@@ -7,6 +7,19 @@ import { LinearGradient } from "expo-linear-gradient";
 import { FontAwesome } from '@expo/vector-icons';
 
 export default function Categories(props) {
+
+    const allTopicList = [
+        { "name": "HTML" },
+        { "name": "CSS" },
+        { "name": "React" },
+        { "name": "React Native" },
+        { "name": "MongoDB" },
+        { "name": "Express" },
+        { "name": "Javascript" }];
+
+
+
+
     return (
 
         <View style={StyleGuide.container}>
@@ -19,101 +32,29 @@ export default function Categories(props) {
                 <FontAwesome style={{ marginTop: 10, marginRight: 10 }} name="gear" size={35} color="black" />
             </View>
 
-
+            <Text style={StyleGuide.fontSize}>Sélectionnes tes themes préférés</Text>
             <ScrollView style={{ flex: 1, height: 50 }}>
 
                 <View style={StyleGuide.buttonMargin}>
-                    <Text style={StyleGuide.fontSize}>Sélectionnes tes themes préférés</Text>
-                    <TouchableOpacity onPress={() => props.navigation.navigate('Home')}>
-                        <LinearGradient start={[0, 0.5]}
-                            end={[1, 0.5]}
-                            colors={['#F81C8F', '#FFA353']}
-                            style={{ borderRadius: 40,  width:  Dimensions.get('window').width/1.3 + 6,
-                            height:  Dimensions.get('window').height/11 + 6 }}>
-                            <View style={StyleGuide.buttonGradientToppic}>
-                                <Text style={StyleGuide.button}>HTML</Text>
-                            </View>
-                        </LinearGradient>
-                    </TouchableOpacity>
+                    {allTopicList.map((item, i) => {
+                        return <TouchableOpacity key={i} onPress={() => props.navigation.navigate('Home')}>
+                            <LinearGradient start={[0, 0.5]}
+                                end={[1, 0.5]}
+                                colors={['#F81C8F', '#FFA353']}
+                                style={{
+                                    borderRadius: 40, width: Dimensions.get('window').width / 1.3 + 6,
+                                    height: Dimensions.get('window').height / 11 + 6
+                                }}>
+                                <View style={StyleGuide.buttonGradientToppic}>
+                                    <Text style={StyleGuide.button}>{item.name}</Text>
+                                </View>
+                            </LinearGradient>
+                        </TouchableOpacity>
+
+                    })}
                 </View>
 
-                <View style={StyleGuide.buttonMargin}>
-                    <TouchableOpacity onPress={() => props.navigation.navigate('Home')}>
-                        <LinearGradient start={[0, 0.5]}
-                            end={[1, 0.5]}
-                            colors={['#F81C8F', '#FFA353']}
-                            style={{ borderRadius: 40 }}>
-                            <View style={StyleGuide.buttonGradientFilled}>
-                                <Text style={StyleGuide.button}>CSS</Text>
-                            </View>
-                        </LinearGradient>
-                    </TouchableOpacity>
-                </View>
 
-                <View style={StyleGuide.buttonMargin}>
-                    <TouchableOpacity onPress={() => props.navigation.navigate('Home')}>
-                        <LinearGradient start={[0, 0.5]}
-                            end={[1, 0.5]}
-                            colors={['#F81C8F', '#FFA353']}
-                            style={{ borderRadius: 40 }}>
-                            <View style={StyleGuide.buttonGradientToppic}>
-                                <Text style={StyleGuide.button}>Javascript</Text>
-                            </View>
-                        </LinearGradient>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={StyleGuide.buttonMargin}>
-                    <TouchableOpacity onPress={() => props.navigation.navigate('Home')}>
-                        <LinearGradient start={[0, 0.5]}
-                            end={[1, 0.5]}
-                            colors={['#F81C8F', '#FFA353']}
-                            style={{ borderRadius: 40 }}>
-                            <View style={StyleGuide.buttonGradientToppic}>
-                                <Text style={StyleGuide.button}>React</Text>
-                            </View>
-                        </LinearGradient>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={StyleGuide.buttonMargin}>
-                    <TouchableOpacity onPress={() => props.navigation.navigate('Home')}>
-                        <LinearGradient start={[0, 0.5]}
-                            end={[1, 0.5]}
-                            colors={['#F81C8F', '#FFA353']}
-                            style={{ borderRadius: 40 }}>
-                            <View style={StyleGuide.buttonGradientToppic}>
-                                <Text style={StyleGuide.button}>React Native</Text>
-                            </View>
-                        </LinearGradient>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={StyleGuide.buttonMargin}>
-                    <TouchableOpacity onPress={() => props.navigation.navigate('Home')}>
-                        <LinearGradient start={[0, 0.5]}
-                            end={[1, 0.5]}
-                            colors={['#F81C8F', '#FFA353']}
-                            style={{ borderRadius: 40 }}>
-                            <View style={StyleGuide.buttonGradientToppic}>
-                                <Text style={StyleGuide.button}>MongoDB</Text>
-                            </View>
-                        </LinearGradient>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={StyleGuide.buttonMargin}>
-                    <TouchableOpacity onPress={() => props.navigation.navigate('Home')}>
-                        <LinearGradient start={[0, 0.5]}
-                            end={[1, 0.5]}
-                            colors={['#F81C8F', '#FFA353']}
-                            style={{ borderRadius: 40 }}>
-                            <View style={StyleGuide.buttonGradientToppic}>
-                                <Text style={StyleGuide.button}>Express</Text>
-                            </View>
-                        </LinearGradient>
-                    </TouchableOpacity>
-                </View>
 
                 <View style={StyleGuide.buttonMargin}>
                     <TouchableOpacity>
