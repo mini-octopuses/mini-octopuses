e31c117509bb1c94fada8416c0e1c7344f48e4df
Vasco <vascbarreiros@gmail.com>
2022-03-07 15:23:42 +0100

    setting fix

--- frontend/components/SwitchComponent.jsx
+++ frontend/components/SwitchComponent.jsx
@@ -6,6 +6,7 @@ export const SwitchComponent = (props) => {
   return (
     <View>
       <Switch
+      style={{marginLeft:40}}
         trackColor={{ false: "#fff", true: "#fff" }}
         thumbColor={props.isEnabled ? "#FFA353" : "#fff"}
         ios_backgroundColor="#3e3e3e"
